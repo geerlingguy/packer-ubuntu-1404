7cc6de0f50a1124a7e11ba007747fa32135e8263
Jeff Geerling <geerlingguy@mac.com>
2014-11-08 00:00:36 -0600

    Only remove .sh files.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Delete unneeded files.
-rm -f /home/vagrant/*
+rm -f /home/vagrant/*.sh
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
