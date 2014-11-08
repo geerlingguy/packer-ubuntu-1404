3501843a3605080b7be6ea42e91955e44814b26f
Jeff Geerling <geerlingguy@mac.com>
2014-11-07 22:49:32 -0600

    General cleanup and minimalization.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Install Ansible repository.
-apt-get -y update
+apt-get -y update && apt-get -y upgrade
 apt-get -y install software-properties-common
 apt-add-repository ppa:ansible/ansible
 
