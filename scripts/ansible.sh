aeae001f94be20dbf3f713598d993dfc6e946b34
Jeff Geerling <geerlingguy@mac.com>
2014-11-07 20:29:42 -0600

    Use official Ansible apt repository.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -3,7 +3,7 @@
 # Install Ansible repository.
 apt-get -y update
 apt-get -y install software-properties-common
-apt-add-repository ppa:rquillo/ansible
+apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
 apt-get -y update
