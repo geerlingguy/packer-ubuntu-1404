4a41dc1b66c677fb387967215f0580a5e6628983
Jeff Geerling <geerlingguy@mac.com>
2015-09-20 21:20:23 -0500

    Fixes #11: Make sure all Ansible-related packages are removed.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,6 +3,7 @@
 # Uninstall Ansible and remove PPA.
 apt-get -y remove --purge ansible
 apt-add-repository --remove ppa:ansible/ansible
+apt-get autoremove
 apt-get update
 
 # Delete unneeded files.
